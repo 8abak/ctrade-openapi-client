f6165172f73f4848779ca5e39ec0d9641b66d522
8abak <52848936+8abak@users.noreply.github.com>
2025-06-21 07:05:23 +1000

    Homepy

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 st.title("Welcome to cTrade Toolkit")
-
+ 
 st.markdown("""
 ### 📊 Available Tools:
 - Use the sidebar to navigate between:
